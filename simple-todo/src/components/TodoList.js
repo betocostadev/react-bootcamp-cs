f4fb1ab9e7a052ef24b4cb23eca8578a097312f1
betocostadev <robertomarx@gmail.com>
2020-06-26 09:05:38 -0300

    added edit to todos

--- simple-todo/src/components/TodoList.js
+++ simple-todo/src/components/TodoList.js
@@ -7,10 +7,11 @@ class TodoList extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { todos: [ ]}
+    this.state = { todos: [] }
 
     this.create = this.create.bind(this)
     this.remove = this.remove.bind(this)
+    this.updateTodo = this.updateTodo.bind(this)
   }
 
   create(newTodo) {
@@ -25,9 +26,22 @@ class TodoList extends Component {
     })
   }
 
+  updateTodo(id, update) {
+    const updatedTodos = this.state.todos.map(todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          task: update
+        }
+      }
+      return todo
+    })
+    this.setState({ todos: updatedTodos })
+  }
+
   render () {
     // Also passing the id as the key, this way the Todo component will have access to the id that matches the key.
-    const todos = this.state.todos.map(td => <Todo key={td.id} id={td.id} task={td.task} removeTodo={this.remove} />)
+    const todos = this.state.todos.map(td => <Todo key={td.id} id={td.id} task={td.task} removeTodo={this.remove} updateTodo={this.updateTodo}/>)
 
     return (
       <div>
